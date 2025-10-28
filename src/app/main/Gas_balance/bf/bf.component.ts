ef060bb993d71bfadcb49b1a01f0e10ad5e1c8a4
Sneha Sadhu <snehasadhucstit33@gmail.com>
2025-10-28 16:51:48 +0530

    commit by sourav

--- src/app/main/Gas_balance/bf/bf.component.ts
+++ src/app/main/Gas_balance/bf/bf.component.ts
@@ -15,78 +15,78 @@ export class BfComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  selectedNode: any = null;
+  // selectedNode: any = null;
 
-  nodes: any = [
-    {
-      name: 'BF#5',
-      cssClass: 'ngx-org-head',
-      childs: [
-        {
-          name: 'BLAST VOL',
-          cssClass: 'ngx-org-body_orange',
-          title: '(NM₃/MIN)',
-          childs: [
-            {
+  // nodes: any = [
+  //   {
+  //     name: 'BF#5',
+  //     cssClass: 'ngx-org-head',
+  //     childs: [
+  //       {
+  //         name: 'BLAST VOL',
+  //         cssClass: 'ngx-org-body_orange',
+  //         title: '(NM₃/MIN)',
+  //         childs: [
+  //           {
 
-              name: 'MAKE',
-              cssClass: 'ngx-org-body_orange',
-              title: '(TNm₃/hr)',
-              childs: [
-                {
-                  name: 'STOVE',
-                  cssClass: 'ngx-org-body_green',
-                  title: '(TOTAL)',
-                  childs: [
-                    { name: 'STOVE1', cssClass: 'ngx-org-body_green' },
-                    { name: 'STOVE2', cssClass: 'ngx-org-body_green' },
-                    { name: 'STOVE3', cssClass: 'ngx-org-body_green' }
-                  ]
-                },
-                {
-                  name: 'PBS#2',
-                  cssClass: 'ngx-org-body',
-                  title: '(TOTAL)',
-                  childs: [
-                    { name: 'BOILER1', cssClass: 'ngx-org-body' },
-                    { name: 'BOILER2', cssClass: 'ngx-org-body' },
-                    { name: 'BOILER3', cssClass: 'ngx-org-body' }
-                  ]
-                },
-                {
-                  name: 'COB#11',
-                  cssClass: 'ngx-org-body_redorange',
-                  title: '(TOTAL)',
-                  childs: [
-                    { name: 'N1 BLOCK', cssClass: 'ngx-org-body_redorange' },
-                    { name: 'N2 BLOCK', cssClass: 'ngx-org-body_redorange' }
-                  ]
-                },
-                {
-                  name: 'FLARE',
-                  cssClass: 'ngx-org-body_violet'
-                },
-                {
-                  name: "MILL'S",
-                  cssClass: 'ngx-org-body_yellow'
-                }
-              ],
-            },
-            {
-              name: 'PRESSURE',
-              cssClass: 'ngx-org-body_orange',
-              title: '(MMWC)'
-            }
-          ]
-        }
-      ]
-    }
-  ];
+  //             name: 'MAKE',
+  //             cssClass: 'ngx-org-body_orange',
+  //             title: '(TNm₃/hr)',
+  //             childs: [
+  //               {
+  //                 name: 'STOVE',
+  //                 cssClass: 'ngx-org-body_green',
+  //                 title: '(TOTAL)',
+  //                 childs: [
+  //                   { name: 'STOVE1', cssClass: 'ngx-org-body_green' },
+  //                   { name: 'STOVE2', cssClass: 'ngx-org-body_green' },
+  //                   { name: 'STOVE3', cssClass: 'ngx-org-body_green' }
+  //                 ]
+  //               },
+  //               {
+  //                 name: 'PBS#2',
+  //                 cssClass: 'ngx-org-body',
+  //                 title: '(TOTAL)',
+  //                 childs: [
+  //                   { name: 'BOILER1', cssClass: 'ngx-org-body' },
+  //                   { name: 'BOILER2', cssClass: 'ngx-org-body' },
+  //                   { name: 'BOILER3', cssClass: 'ngx-org-body' }
+  //                 ]
+  //               },
+  //               {
+  //                 name: 'COB#11',
+  //                 cssClass: 'ngx-org-body_redorange',
+  //                 title: '(TOTAL)',
+  //                 childs: [
+  //                   { name: 'N1 BLOCK', cssClass: 'ngx-org-body_redorange' },
+  //                   { name: 'N2 BLOCK', cssClass: 'ngx-org-body_redorange' }
+  //                 ]
+  //               },
+  //               {
+  //                 name: 'FLARE',
+  //                 cssClass: 'ngx-org-body_violet'
+  //               },
+  //               {
+  //                 name: "MILL'S",
+  //                 cssClass: 'ngx-org-body_yellow'
+  //               }
+  //             ],
+  //           },
+  //           {
+  //             name: 'PRESSURE',
+  //             cssClass: 'ngx-org-body_orange',
+  //             title: '(MMWC)'
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ];
 
-  onNodeClick(event: any) {
-    this.selectedNode = event;
-    console.log('Selected Node:', event);
-  }
+  // onNodeClick(event: any) {
+  //   this.selectedNode = event;
+  //   console.log('Selected Node:', event);
+  // }
 
 
 
